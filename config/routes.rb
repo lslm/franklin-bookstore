955c06372d9000b61fe2aaccbddbf1959976db2b
Lucas Santos <lucas.santos@plataformatec.com.br>
2019-03-28 23:44:51 -0300

    Added Users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :categories
   resources :price_groups
   resources :products
