0783e6a3d31a3650615841c64f0cc6258aa99c0f
Lucas Santos <lucas.santos@plataformatec.com.br>
2019-05-27 00:35:25 -0300

    Indent using spaces instead of tabs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,92 +1,85 @@
 class ProductsController < ApplicationController
   before_action :authorize
-	before_action :authorize_admin
+  before_action :authorize_admin
 
-	def index
+  def index
     @products = Product.all.order(params[:sort])
-	end
+  end
 
-	def show
-		@product = Product.find(params[:id])
-	end
+  def show
+    @product = Product.find(params[:id])
+  end
 
-	def new
-		@product = current_user.products.build
-	end
+  def new
+    @product = current_user.products.build
+  end
 
-	def create
-		@product = current_user.products.build(products_params)
-		
-		respond_to do |format|
-			if @product.save
+  def create
+    @product = current_user.products.build(products_params)
 
-				if params[:images]
-					params[:images].each { |image|
-						@product.pictures.create(image: image, imageable_id: @product.id)
-					}
-				end
+    respond_to do |format|
+      if @product.save
+        if params[:images]
+          params[:images].each { |image|
+            @product.pictures.create(image: image, imageable_id: @product.id)
+          }
+        end
 
-				format.html { redirect_to @product, notice: 'Product was successfully created.' }
-				format.json { render json: @product, status: :created, location: @product }
-			else
-				format.html { render action: "new" }
-				format.json { render json: @product.errors, status: :unprocessable_entity }
-			end
-		end
-	end
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render json: @product, status: :created, location: @product }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def edit
     @product = Product.find(params[:id])
-	end
+  end
 
   def update
     @product = Product.find(params[:id])
 
-		if @product.update(products_params)
-			# Check if any new images have been added
-			if params[:images]
-				params[:images].each do |image|
-					# Create the images
-					@product.pictures.create(image: image, imageable_id: @product.id)
-				end
-			end
-			redirect_to products_path
-		else
-			render "edit"
-		end
-	end
+    if @product.update(products_params)
+      # Check if any new images have been added
+      if params[:images]
+        params[:images].each do |image|
+          # Create the images
+          @product.pictures.create(image: image, imageable_id: @product.id)
+        end
+      end
+      redirect_to products_path
+    else
+      render "edit"
+    end
+  end
 
-	def destroy
+  def destroy
     product = Product.find(params[:id])
-		product.destroy
-		redirect_to products_path
-	end
+    product.destroy
+    redirect_to products_path
+  end
 
 	private
 
-	def products_params
-		params.require(:product).permit(
-			:name,
-			:description,
-			:price,
-			:old_price,
-			:on_sale,
-			:quantity,
-			:sold_out,
-			:featured,
-			:category_id,
-			:pictures,
-			:author,
-    	:year,
-    	:title,
-    	:publisher,
-    	:edition,
-    	:isbn,
-    	:pages,
-    	:dimensions,
-    	:barcode,
-    	:active,
-    	:activation_reason,
-			pictures_attributes: [:id, :image, :_destroy])
-	end
+  def products_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :pictures,
+      :author,
+      :year,
+      :title,
+      :publisher,
+      :edition,
+      :isbn,
+      :pages,
+      :dimensions,
+      :barcode,
+      :active,
+      :activation_reason,
+      pictures_attributes: [:id, :image, :_destroy])
+  end
 end
