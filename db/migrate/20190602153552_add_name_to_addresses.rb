a088de513d03de4c016472c8733efd9266290a03
Lucas Santos <lucas.santos@plataformatec.com.br>
2019-06-02 12:46:13 -0300

    Add name field to addresses

---
+++ db/migrate/20190602153552_add_name_to_addresses.rb
@@ -0,0 +1,5 @@
+class AddNameToAddresses < ActiveRecord::Migration[5.2]
+  def change
+    add_column :addresses, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_135058) do
+ActiveRecord::Schema.define(version: 2019_06_02_153552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_06_02_135058) do
     t.string "address_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
