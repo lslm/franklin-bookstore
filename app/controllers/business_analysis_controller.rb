cf4f2fcbadc2841628f30801b71f03d5c56d367b
Lucas Santos <lucas.santos@plataformatec.com.br>
2019-06-18 15:35:28 -0300

    Added line chart

--- app/controllers/business_analysis_controller.rb
+++ app/controllers/business_analysis_controller.rb
@@ -1,5 +1,8 @@
 class BusinessAnalysisController < ApplicationController
   def index
     @data = Category.total_by_quantity
+
+    @books = Product.all.order(:name).pluck(:name)
+    @line_chart_data = Checkout.count_items_sold
   end
 end
--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -5,4 +5,44 @@ class Checkout < ApplicationRecord
   def total
     self.order.order_items.pluck(:total_price).reduce(:+)
   end
+
+  def self.count_items_sold
+    data = Checkout.find_by_sql(<<-SQL
+      select TO_CHAR(checkouts.created_at :: DATE, 'dd/mm/yyyy') as date,
+      products."name",
+      sum(order_items.quantity) as quantity
+      from checkouts
+      join order_items on order_items.order_id = checkouts.order_id
+      join stocks on stocks.id = order_items.stock_id
+      join products on products.id = stocks.product_id
+      group by 1,2
+    SQL
+    ).map do |row|
+      {dia: row['date'], nome: row['name'], quantidade: row['quantity']}
+    end
+
+    data = data.group_by { |i| i[:dia]}.to_a
+
+    data.map do |d|
+      [d[0]] + formalize_data(d)
+    end
+  end
+
+  def self.formalize_data(data)
+    books = Product.order(:name).pluck(:name)
+
+    sold_books = data[1].map do |item|
+      {item[:nome] => item[:quantidade]}
+    end
+
+    sold_books = sold_books.inject(:merge)
+
+    books.map do |book|
+      if sold_books.key?(book)
+        sold_books[book]
+      else
+        0
+      end
+    end
+  end
 end
