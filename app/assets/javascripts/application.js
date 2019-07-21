94c38cc3ec8003febff816c7d82108efbfb3fdb8
Lucas Santos <lucas.santos@plataformatec.com.br>
2019-07-20 23:07:09 -0300

    Remove old style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,4 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
-//= require chart
-//= require dashboard
-//= require maps
-//= require misc
-//= require off-canvas
 //= require font-awesome-all
