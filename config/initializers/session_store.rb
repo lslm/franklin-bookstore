e6072675aaff6c98344fbc622e7481276e7a73fa
Lucas Santos <lucas.santos@plataformatec.com.br>
2019-08-20 19:11:10 -0300

    Remove generic name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ecommerce_session'
+Rails.application.config.session_store :cookie_store, key: '_bookstore_session'
